d67111a421779530a67153ebb5f348008838828e
Allana Creech <allanacreech@gmail.com>
2017-01-31 18:36:26 -0500

    bootstrap now working

--- server.js
+++ server.js
@@ -2,23 +2,23 @@
 //++++++++++++++++++++++++++++++++++++++++++++++++++++
 var express = require("express");
 var bodyParser = require("body-parser");
-var mysql = require("mysql");
-var exphndl = require("express-handlebars");
+// var mysql = require("mysql");
+// var exphndl = require("express-handlebars");
 var method_override = require("method-override");
 
-var port = process.env.NODE_ENV || 3000;
+var port = process.env.PORT || 3000;
 
 var app = express();
 
 app.use(express.static(process.cwd() + "/public"));
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended:false }));
 
 app.use(method_override("_method"));
 
 
 var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({defautlLayout: "main"}));
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
 
 app.set("view engine", "handlebars");
 
